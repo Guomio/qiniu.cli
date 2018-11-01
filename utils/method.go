0d748ac34546969ad685915c5f0e8acdd4c8a55d
Guo <839665066@qq.com>
2018-11-01 09:50:03 +0800

    Update method.go

--- utils/method.go
+++ utils/method.go
@@ -17,13 +17,13 @@ func GetAbsPath(s string) string {
 
 // GetFileSize 获取文件大小
 func GetFileSize(n int64) string {
-	table := []string{"B", "KB", "MB", "GB", "GB", "PB", "EB", "ZB", "YB"}
+	table := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
 	i := 0
 	for n>>10 >= 1 {
 		n >>= 10
 		i++
 	}
-	if i > len(table) {
+	if i >= len(table) {
 		return "too big!!!"
 	}
 	return strconv.FormatInt(n, 10) + " " + table[i]
